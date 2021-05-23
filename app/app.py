e6ee881fcbc42106eaf5858e77c69e0bb2d742ba
wiktorkisielewski <wiktor.kisielewski@protonmail.com>
2021-05-23 09:06:51 +0200

    app 0.2

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import psycopg2
+import datetime
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
@@ -10,8 +11,13 @@ def main():
 @app.route('/post_to_psql_db', methods=['POST', 'GET'])
 def post_it():
 	con = psycopg2.connect(database="database", user="admin", password="admin1", host="senti1_db", port=5432)
+	cur = con.cursor()
 	if request.method == 'POST':
 	    data = request.form['n']
+	    cur.execute("INSERT INTO psqldata (RESPONSE, TIME_STAMP) VALUES ('{}', '{}');".format(str(data), str(datetime.datetime.now())))
+	    con.commit()
+	    print('data saved')
+	    con.close()
 	    return redirect(url_for('main'))
 
 if __name__ == '__main__':
